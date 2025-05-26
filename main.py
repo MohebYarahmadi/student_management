df1b1fc13c5a193f0c953e169c01a1180eba716b
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2025-05-26 08:45:23 +0330

    Menubar added to the mainWindow

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
-from PyQT6.QtWidgets import QApplication, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, \
+    QMainWindow, QLabel, QLineEdit, QPushButton
+from PyQt6.QtGui import QAction
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management")\
+
+        # Create Menubar
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        # Add submenu for each menubar
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About Manager", self)
+        help_menu_item.addAction(about_action)
+        # about_action.setMenuRole(QAction.MenuRole.NoRole) # Fix Some Mac Issue
+
+        
 
 def main():
-    pass
+    app = QApplication(sys.argv)
+    manager = MainWindow()
+    manager.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
